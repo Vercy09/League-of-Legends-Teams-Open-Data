27fc05b8d5e54ae9e30410197f26b69a4ddea6d8
Vercy09 <lukapro.9122000@gmail.com>
2021-11-08 19:47:44 +0100

    Added routes and views

--- server.js
+++ server.js
@@ -4,3 +4,24 @@ const app = express();
 const path = require('path');
 const pg = require('pg');
 const db = require('./db');
+
+//uvoz modula s definiranom funkcionalnosti ruta
+const indexRouter = require('./routes/index.routes');
+const datatableRouter = require('./routes/datatable.routes');
+
+//middleware - predlošci (ejs)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//middleware - statički resursi
+app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware - dekodiranje parametara
+app.use(express.urlencoded({ extended: true }));
+
+//definicija ruta
+app.use('/', indexRouter);
+app.use('/data', datatableRouter);
+
+//pokretanje poslužitelja na portu 3000
+app.listen(3000);
